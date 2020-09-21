8591a6569b15f4364bdc7e3acbf3b4bce3055c05
Killer-Hacker-Oficial <killer_hacker_oficial_98@protnmail.com>
2020-09-21 08:39:38 -0600

    Upload

---
+++ autobuild.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# gpg --detach-sign killer-os.iso
+# gpg --verify killer-os.iso.sig killer-os.iso
+
+iso_name=killer-os
+iso_de=$1
+iso_version=$(date +%Y%m%d)
+script_path=$(realpath -- ${0%/*})
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+img_name="${iso_name}_${iso_de}_${iso_version}.iso"
+
+#Build ISO File
+build_iso(){
+  pacman -Scc --noconfirm --quiet
+  rm -rf /var/cache/pacman/pkg/*
+  pacman-key --init
+  pacman-key --populate
+  pacman -Syy --quiet
+
+  source $script_path/mkarchiso -v $script_path
+}
+
+# create md5sum, sha256, sig
+check_sums() {
+  cd out/
+  echo "create MD5, SHA-256 Checksum, sig"
+  md5sum $img_name >> $img_name.md5.txt
+  shasum -a 256 $img_name >> $img_name.sha256.txt
+  # sudo -u ${SUDO_UID} gpg --detach-sign --no-armor $img_name
+  cd ..
+  chown -R "${SUDO_UID}:${SUDO_GID}" $script_path/out
+}
+
+run_qemu()
+{
+  qemu-system-x86_64 -m 2G -boot d -enable-kvm -cdrom $script_path/out/$img_name
+}
+
+build_iso
+check_sums
+# run_qemu
---
+++ chroot.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+script_path=$(readlink -f ${0%/*})
+work_dir=${script_path}/work/x86_64/airootfs
+
+chrooter(){
+   arch-chroot ${work_dir} /bin/bash -c "${1}"
+}
+
+echo "==== create settings.sh ===="
+
+cat <<EOF >${work_dir}/settings.sh
+reflector -a 12 -l 70 -f 30 -p https -p http --sort rate --save /etc/pacman.d/mirrorlist
+
+pacman-key --init
+pacman-key --populate
+pacman-key --refresh-keys
+pacman -Syyuu --noconfirm
+
+## blackarch strap
+curl -OL https://blackarch.org/strap.sh
+sed -i "s|pgp.mit.edu|keys.gnupg.net|g" strap.sh
+chmod +x strap.sh
+./strap.sh
+rm strap.sh
+
+pacman-key --init
+pacman-key --populate
+pacman-key --refresh-keys
+pacman -Syyuu --noconfirm
+
+sed -i '/User/s/^#\+//' /etc/sddm.conf
+
+Lsb_release() {
+    sed -i /etc/lsb-release \
+        -e 's,DISTRIB_ID=.*,DISTRIB_ID=Killer-OS,' \
+        -e 's,DISTRIB_RELEASE=.*,DISTRIB_RELEASE=Rolling,' \
+        -e 's,DISTRIB_CODENAME=.*,DISTRIB_CODENAME=Killer-Hacker-Oficial,' \
+        -e 's,DISTRIB_DESCRIPTION=.*,DISTRIB_DESCRIPTION=\"Killer-OS Linux\",'
+
+}
+
+Os_release() {
+    sed -i /usr/lib/os-release \
+        -e 's,NAME=.*,NAME=\"Killer-OS Linux\",' \
+        -e 's,PRETTY_NAME=.*,PRETTY_NAME=\"Killer-OS Linux\",' \
+        -e 's,ID=.*,ID=killer-os,' \
+        -e 's,ID_LIKE=.*,ID_LIKE=arch,' \
+        -e 's,BUILD_ID=.*,BUILD_ID=rolling,' \
+        -e 's,HOME_URL=.*,HOME_URL=\"https://killer-os-oficial.github.io\",' \
+        -e 's,DOCUMENTATION_URL=.*,DOCUMENTATION_URL=\"https://killer-os-oficial.github.io/wiki\",' \
+        -e 's,SUPPORT_URL=.*,SUPPORT_URL=\"https://t.me/Killer_OS_Info\",' \
+        -e 's,BUG_REPORT_URL=.*,BUG_REPORT_URL=\"https://github.com/Killer-OS-Oficial/Killer-OS/issues\",' \
+        -e 's,LOGO=.*,LOGO=killer-os,'
+}
+
+Issues() {
+    sed -i 's|Arch|Killer-OS|g' /etc/issue /usr/share/factory/etc/issue
+}
+Lsb_release
+Os_release
+Issues
+
+
+# sed -i 's?GRUB_DISTRIBUTOR=.*?GRUB_DISTRIBUTOR=\"Killer-OS\"?' /etc/default/grub
+# sed -i 's?\#GRUB_THEME=.*?GRUB_THEME=\/boot\/grub\/themes\/crimson\/theme.txt?g' /etc/default/grub
+# echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
+EOF
+
+chmod +x ${work_dir}/settings.sh
+chrooter /settings.sh
+rm ${work_dir}/settings.sh
+
+echo "==== Done settings.sh ===="
